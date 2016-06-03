d11b1c2082abb5a7c348b194afb6aedf3d60bf6c
linezero <linezero@qq.com>
2016-06-03 11:32:13 +0800

    .NET Core Logging
    .NET Core Logging And NLog Sample

---
+++ NETCoreLogging/src/NETCoreLogging/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NETCoreLogging.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            _logger.LogInformation("你访问了首页");
+            _logger.LogWarning("警告信息");
+            _logger.LogError("错误信息");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
