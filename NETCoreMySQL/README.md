05ba62ecd46fed102fba04fa859dbdde23c75f83
linezero <linezero@qq.com>
2016-08-25 14:57:37 +0800

    Update NETCoreMySQL

--- NETCoreMySQL/README.md
+++ NETCoreMySQL/README.md
@@ -1,3 +1,5 @@
 #NETCoreMySQL
 
 [.NET Core 使用Dapper 操作MySQL](http://www.cnblogs.com/linezero/p/NETCoreMySQL.html)
+
+[MySQL官方.NET Core驱动已出，支持EF Core](http://www.cnblogs.com/linezero/p/5806814.html)
--- NETCoreMySQL/src/NETCoreMySQL/Program.cs
+++ NETCoreMySQL/src/NETCoreMySQL/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
 using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace NETCoreMySQL
 {
@@ -12,16 +14,16 @@ namespace NETCoreMySQL
     {
         public static void Main(string[] args)
         {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            MySqlConnection con = new MySqlConnection("server=127.0.0.1;database=test;uid=root;pwd=;charset='gbk'");
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);           
+            MySqlConnection con = new MySqlConnection("server=127.0.0.1;database=test;uid=root;pwd=;charset='gbk';SslMode=None");
             //新增数据
             con.Execute("insert into user values(null, '测试', 'http://www.cnblogs.com/linezero/', 18)");
             //新增数据返回自增id
-            var id=con.QueryFirst<int>("insert into user values(null, 'linezero', 'http://www.cnblogs.com/linezero/', 18);select last_insert_id();");
+            var id = con.QueryFirst<int>("insert into user values(null, 'linezero', 'http://www.cnblogs.com/linezero/', 18);select last_insert_id();");
             //修改数据
             con.Execute("update user set UserName = 'linezero123' where Id = @Id", new { Id = id });
             //查询数据
-            var list=con.Query<User>("select * from user");
+            var list = con.Query<User>("select * from user");
             foreach (var item in list)
             {
                 Console.WriteLine($"用户名：{item.UserName} 链接：{item.Url}");
